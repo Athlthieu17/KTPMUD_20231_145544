026adc86dd5ce4a30556da74bf3b4589b666181e
Lê Trung Hiếu <112858605+Athlthieu17@users.noreply.github.com>
2024-01-31 00:19:55 +0700

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,17 @@ from .api import apirouter
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = [
+    "*",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=apirouter.router, prefix="/api")
 
